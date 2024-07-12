ee4f20d1b0299aa7cc3bd5f3371a07467377c05a
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2024-07-12 11:15:42 +0300

    deployment changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import Navbar from "@/Components/Commons/Navbar";
 import LargeScreenCard from "@/Components/LargeScreenCard";
 import SmallScreenCard from "@/Components/SmallScreenCard";
@@ -8,7 +6,7 @@ import SmallScreenCard from "@/Components/SmallScreenCard";
 export default function Home() {
   return (
 <>
-<div className="hidden md:flex px-[24px] xl:px-[48px] w-screen h-screen relative">
+<div className="hidden md:flex px-[24px] xl:px-[48px] w-screen h-screen ">
      <LargeScreenCard /> 
 </div>
 <div className="grid md:hidden h-full gap-2.5 bg-[#F4F8FB]">
